5db3e88c066bde2f8a939ce4b962c682d1b044e2
Thomas Schreiber <thomas.schreiebr@gmx.at>
2014-10-31 11:23:16 +0100

    Initial Commit

---
+++ src/main/java/at/tuwien/aic/tweetanalysis/TweetAnalysis.java
@@ -0,0 +1,28 @@
+/*
+ * Tweet Sentiment Analysis
+ * Vienna UT
+ *
+ * Advanced Internet Computing
+ * Winter term 2014/15
+ * Group 01
+ * 
+ * Patrick Löwenstein
+ * Thomas Schreiber
+ * Alexander Suchan
+ * Stefan Victoria
+ * Andreas Waltenberg
+ *
+ * All rights reserved
+ */
+
+package at.tuwien.aic.tweetanalysis;
+
+/**
+ *
+ * @author Thomas
+ */
+public class TweetAnalysis {
+    public static void main (String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/at/tuwien/aic/tweetanalysis/TweetAnalysisTest.java
@@ -0,0 +1,25 @@
+/*
+ */
+
+package at.tuwien.aic.tweetanalysis;
+
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Thomas
+ */
+public class TweetAnalysisTest {
+    
+
+    /**
+     * Test of main method, of class TweetAnalysis.
+     */
+    @Test
+    public void testMain() {
+        assertEquals(1, 1);
+    }
+    
+}
