a5c3a0dc18019ff1b5ec4c8db657df926b385568
Stefan Victora <Stefan.Victora@gmail.com>
2014-11-27 13:30:16 +0100

    added some methods in the main method for live testing

--- src/main/java/at/tuwien/aic/tweetanalysis/TweetAnalysis.java
+++ src/main/java/at/tuwien/aic/tweetanalysis/TweetAnalysis.java
@@ -20,6 +20,16 @@ package at.tuwien.aic.tweetanalysis;
 import at.tuwien.aic.tweetanalysis.classifier.IClassifier;
 import at.tuwien.aic.tweetanalysis.classifier.WekaClassifier;
 import at.tuwien.aic.tweetanalysis.entities.Tweet;
+import at.tuwien.aic.tweetanalysis.preprocessing.StandardTweetPreprocessor;
+import at.tuwien.aic.tweetanalysis.provider.TweetProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Future;
 
 /**
  *
@@ -29,22 +39,18 @@ import at.tuwien.aic.tweetanalysis.entities.Tweet;
  */
 public class TweetAnalysis {
 
+    public static final Logger log = LoggerFactory.getLogger(TweetAnalysis.class);
 
     public static void main(String[] args) throws Exception {
 
         System.out.println("WEKA Test!");
 
+//        testClassifier();
+        testLiveData();
 
-        IClassifier tweetTest = new WekaClassifier();
+//        NaiveTweetPreprocessor naiveTweetPreprocessor = new NaiveTweetPreprocessor();
 
-        // Use the Classifier to evaluate a Tweet
-        Tweet t = new Tweet();
-        t.setContent("microsoft sucks bad :(");
-        double[] fDistribution = tweetTest.classifyTweet(t);
-
-        System.out.println("Evaluation of a String: " + t.getContent());
-        System.out.println("probability of being positive: " + fDistribution[0]);
-        System.out.println("probability of being negative: " + fDistribution[1]);
+//        log.trace("{}", naiveTweetPreprocessor);
 
         // INFO:
         // Ignore the database errors, only important if we use a database for the tweets.
@@ -56,6 +62,42 @@ public class TweetAnalysis {
         // TweetProvider provider = new TweetProvider();
         // List<Tweet> tweets = provider.getTweets("test", 20, new Date(114, 10, 18), new Date(114, 10, 20)).get();
         // System.out.println("Got " + tweets.size() + " tweets");
+    }
+
+    private static void testLiveData() throws InterruptedException, java.util.concurrent.ExecutionException {
+        TweetProvider tweetProvider = new TweetProvider();
+
+        Future<List<Tweet>> tweets = tweetProvider.getTweets(":)", 20, null, null);
+
+        List<Tweet> tweetList = tweets.get();
+
+        StandardTweetPreprocessor standardTweetPreprocessor = new StandardTweetPreprocessor();
+        standardTweetPreprocessor.preprocess(tweetList);
+
+        for (Tweet tweet : tweetList) {
+            log.info("{}", tweet);
+        }
+    }
+
+    private static void testClassifier() throws IOException {
+        IClassifier tweetTest;
+        try (InputStream modelStream = TweetAnalysis.class.getResourceAsStream("/tweet-model.model")) {
+            tweetTest = new WekaClassifier(modelStream);
+        }
+
+        // Use the Classifier to evaluate a Tweet
+        Tweet t = new Tweet();
+        t.setContent("bad bad bad bad :(");
+
+        StandardTweetPreprocessor standardTweetPreprocessor = new StandardTweetPreprocessor();
+        ArrayList<Tweet> tweets = new ArrayList<>();
+        tweets.add(t);
+        standardTweetPreprocessor.preprocess(tweets);
+        double[] fDistribution = tweetTest.classifyTweet(t);
+
+        System.out.println("Evaluation of a String: " + t.getContent());
+        System.out.println("probability of being positive: " + fDistribution[0]);
+        System.out.println("probability of being negative: " + fDistribution[1]);
     }
 
 
