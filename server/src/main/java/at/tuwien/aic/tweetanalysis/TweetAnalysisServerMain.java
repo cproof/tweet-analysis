5e40c7b0b58c0073cca83d0ebebb205174bcc50e
Thomas Schreiber <thomas.schreiber@gmx.at>
2015-01-03 20:57:32 +0100

    allow detailed transmission of tweets from server

--- server/src/main/java/at/tuwien/aic/tweetanalysis/TweetAnalysisServerMain.java
+++ server/src/main/java/at/tuwien/aic/tweetanalysis/TweetAnalysisServerMain.java
@@ -5,6 +5,7 @@ package at.tuwien.aic.tweetanalysis;
 
 import asg.cliche.Shell;
 import asg.cliche.ShellFactory;
+import static at.tuwien.aic.tweetanalysis.TweetAnalysis.log;
 import static at.tuwien.aic.tweetanalysis.TweetAnalysis.shell;
 import java.io.IOException;
 import java.io.InputStream;
@@ -27,9 +28,11 @@ import at.tuwien.aic.tweetanalysis.entities.ClassifiedTweet;
 import at.tuwien.aic.tweetanalysis.entities.Tweet;
 import at.tuwien.aic.tweetanalysis.preprocessing.StandardTweetPreprocessor;
 import at.tuwien.aic.tweetanalysis.provider.TweetProvider;
+import java.util.Map;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.AbstractHandler;
+import twitter4j.JSONArray;
 import twitter4j.JSONException;
 import twitter4j.JSONObject;
 import twitter4j.RateLimitStatus;
@@ -90,6 +93,7 @@ public class TweetAnalysisServerMain {
         public void handle(String target,Request baseRequest,HttpServletRequest request,HttpServletResponse response) throws IOException, ServletException {
             baseRequest.setHandled(true);
             response.setContentType("application/json;charset=utf-8");            
+            response.setHeader("Access-Control-Allow-Origin", "*");
             
             switch(target) {
                 case "/search":
@@ -118,8 +122,11 @@ public class TweetAnalysisServerMain {
                 try {
                     count = Integer.parseInt(countString);
                 } catch(Exception e) {
-                    count = 100;
+                    count = 10;
                 }
+                
+                
+                boolean showTweets = (baseRequest.getParameter("verbose") != null && !baseRequest.getParameter("verbose").equals("false"));
 
                 RateLimitStatus status = tweetProvider.getRateLimitStatus();
                 if(status != null && status.getRemaining() == 0) {
@@ -146,11 +153,63 @@ public class TweetAnalysisServerMain {
                 }
 
                 ret.put("sentiment", aggregator.calculate(classifiedTweetList));
+                
+                
+                //maybe attach tweets?
+                JSONArray tweetsJSON = null;
+                if (showTweets) {
+                    tweetsJSON = new JSONArray();
+                    for(ClassifiedTweet t : classifiedTweetList) {
+                        tweetsJSON.put(verboseJSON(t.tweet, t.fDistribution, true));
+                    }
+                }
+                ret.put("tweets", tweetsJSON);
+                
                 response.setStatus(HttpServletResponse.SC_OK);
                 response.getWriter().print(ret.toString());
             } catch (JSONException ex) {
                 handleBadRequest(baseRequest, request, response);
             }
+        }
+        
+        /**
+         * Product detailed JSON for a single tweet
+         * (copied from TweetAnalysis:logResults()
+         * @param tweet
+         * @param fDistribution
+         * @param verbose
+         * @return
+         * @throws JSONException 
+         */
+        private static JSONObject verboseJSON(Tweet tweet, double[] fDistribution, boolean verbose) throws JSONException {
+            JSONObject ret = new JSONObject();
+            ret.put("original",tweet.getOriginalContent().replace("\n", ""));
+            ret.put("processed", tweet.getContent());
+            ret.put("positive", fDistribution[1]);
+            ret.put("negative", fDistribution[0]);
+            ret.put("id", tweet.getId());
+            ret.put("author", tweet.getAuthor());
+            
+            int retweetCount = tweet.getRetweetCount();
+            int favoriteCount = tweet.getFavoriteCount();
+            
+            JSONObject features = new JSONObject();
+            if (verbose) { //@TODO: allow different settings
+                for (Map.Entry<String, Double> featureEntry : tweet.getFeatureMap().entrySet()) {
+                    if (featureEntry.getValue() != 0.0) {
+                        features.put(featureEntry.getKey().toLowerCase(), featureEntry.getValue());
+                    }
+                }
+                if (features.length() > 0) {
+                    ret.put("featureMap", features);
+                }
+            }
+            
+            ret.put("retweets", retweetCount);
+            ret.put("favorites", favoriteCount);
+            ret.put("weight", SimpleAggregator.weight(tweet));
+            
+            return ret;
         }
 
         private void handleBadRequest(Request baseRequest,HttpServletRequest request,HttpServletResponse response) throws IOException {
