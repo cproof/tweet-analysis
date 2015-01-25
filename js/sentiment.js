36b9f4fbfead98eb9a0be630c0c78dc0bbe3f59c
Thomas Schreiber <thomas.schreiber@gmx.at>
2015-01-25 14:31:42 +0100

    update to current version

--- js/sentiment.js
+++ js/sentiment.js
@@ -3,7 +3,7 @@ var baseUrl = "http://localhost:8080";
 angular.module('sentiment', ['nvd3'])
         .controller('sentimentSearchController', function ($scope, $http) {
             $scope.query = "";
-            $scope.count = 2;
+            $scope.count = 20;
             $scope.classifier = "smo";
             $scope.language = null;
             $scope.beginDate = null;
@@ -13,7 +13,14 @@ angular.module('sentiment', ['nvd3'])
             $scope.verbose = true;
             
             $scope.countOptions = [2,10,20,50,100,200,500];
-            $scope.classifierOptions = {smo:"SMO",smoSmileys:"SMO using Smileys",bayes:"Naive Bayes",bayesSmileys:"Naive Bayes using Smileys"};
+            $scope.classifierOptions = {smo:"SMO", smo_small:"SMO (small)", smo_large:"SMO (large)",
+                smoSmileys:"SMO using Smileys", smo_small_Smileys:"SMO using Smileys (small)", smo_large_Smileys:"SMO using Smileys (large)",
+                bayes:"Naive Bayes", bayes_small:"Naive Bayes (small)", bayes_large:"Naive Bayes (large)",
+                bayesSmileys:"Naive Bayes using Smileys", bayes_small_Smileys:"Naive Bayes using Smileys (small)", bayes_large_Smileys:"Naive Bayes using Smileys (large)",
+                c_svc:"C-SVC", c_svc_small:"C-SVC (small)", c_svc_large:"C-SVC (large)",
+                c_svcSmileys:"C-SVC using Smileys", c_svc_small_Smileys:"C-SVC using Smileys (small)", c_svc_large_Smileys:"C-SVC using Smileys (large)",
+                nu_svc:"NU-SVC", nu_svc_small:"NU-SVC (small)", nu_svc_large:"NU-SVC (large)",
+                nu_svcSmileys:"NU-SVC using Smileys", nu_svc_small_Smileys:"NU-SVC using Smileys (small)", nu_svc_large_Smileys:"NU-SVC using Smileys (large)"};
             $scope.languageOptions = {ab:"Abkhaz",aa:"Afar",af:"Afrikaans",ak:"Akan",sq:"Albanian",am:"Amharic",ar:"Arabic",an:"Aragonese",hy:"Armenian",as:"Assamese",av:"Avaric",ae:"Avestan",ay:"Aymara",az:"Azerbaijani",bm:"Bambara",ba:"Bashkir",eu:"Basque",be:"Belarusian",bn:"Bengali",bh:"Bihari",bi:"Bislama",bs:"Bosnian",br:"Breton",bg:"Bulgarian",my:"Burmese",ca:"Catalan; Valencian",ch:"Chamorro",ce:"Chechen",ny:"Chichewa; Chewa; Nyanja",zh:"Chinese",cv:"Chuvash",kw:"Cornish",co:"Corsican",cr:"Cree",hr:"Croatian",cs:"Czech",da:"Danish",dv:"Divehi; Dhivehi; Maldivian;",nl:"Dutch",en:"English",eo:"Esperanto",et:"Estonian",ee:"Ewe",fo:"Faroese",fj:"Fijian",fi:"Finnish",fr:"French",ff:"Fula; Fulah; Pulaar; Pular",gl:"Galician",ka:"Georgian",de:"German",el:"Greek, Modern",gn:"Guaraní",gu:"Gujarati",ht:"Haitian; Haitian Creole",ha:"Hausa",he:"Hebrew (modern)",hz:"Herero",hi:"Hindi",ho:"Hiri Motu",hu:"Hungarian",ia:"Interlingua",id:"Indonesian",ie:"Interlingue",ga:"Irish",ig:"Igbo",ik:"Inupiaq",io:"Ido",is:"Icelandic",it:"Italian",iu:"Inuktitut",ja:"Japanese",jv:"Javanese",kl:"Kalaallisut, Greenlandic",kn:"Kannada",kr:"Kanuri",ks:"Kashmiri",kk:"Kazakh",km:"Khmer",ki:"Kikuyu, Gikuyu",rw:"Kinyarwanda",ky:"Kirghiz, Kyrgyz",kv:"Komi",kg:"Kongo",ko:"Korean",ku:"Kurdish",kj:"Kwanyama, Kuanyama",la:"Latin",lb:"Luxembourgish, Letzeburgesch",lg:"Luganda",li:"Limburgish, Limburgan, Limburger",ln:"Lingala",lo:"Lao",lt:"Lithuanian",lu:"Luba-Katanga",lv:"Latvian",gv:"Manx",mk:"Macedonian",mg:"Malagasy",ms:"Malay",ml:"Malayalam",mt:"Maltese",mi:"Māori",mr:"Marathi (Marāṭhī)",mh:"Marshallese",mn:"Mongolian",na:"Nauru",nv:"Navajo, Navaho",nb:"Norwegian Bokmål",nd:"North Ndebele",ne:"Nepali",ng:"Ndonga",nn:"Norwegian Nynorsk",no:"Norwegian",ii:"Nuosu",nr:"South Ndebele",oc:"Occitan",oj:"Ojibwe, Ojibwa",cu:"Old Church Slavonic, Church Slavic, Church Slavonic, Old Bulgarian, Old Slavonic",om:"Oromo",or:"Oriya",os:"Ossetian, Ossetic",pa:"Panjabi, Punjabi",pi:"Pāli",fa:"Persian",pl:"Polish",ps:"Pashto, Pushto",pt:"Portuguese",qu:"Quechua",rm:"Romansh",rn:"Kirundi",ro:"Romanian, Moldavian, Moldovan",ru:"Russian",sa:"Sanskrit (Saṁskṛta)",sc:"Sardinian",sd:"Sindhi",se:"Northern Sami",sm:"Samoan",sg:"Sango",sr:"Serbian",gd:"Scottish Gaelic; Gaelic",sn:"Shona",si:"Sinhala, Sinhalese",sk:"Slovak",sl:"Slovene",so:"Somali",st:"Southern Sotho",es:"Spanish; Castilian",su:"Sundanese",sw:"Swahili",ss:"Swati",sv:"Swedish",ta:"Tamil",te:"Telugu",tg:"Tajik",th:"Thai",ti:"Tigrinya",bo:"Tibetan Standard, Tibetan, Central",tk:"Turkmen",tl:"Tagalog",tn:"Tswana",to:"Tonga (Tonga Islands)",tr:"Turkish",ts:"Tsonga",tt:"Tatar",tw:"Twi",ty:"Tahitian",ug:"Uighur, Uyghur",uk:"Ukrainian",ur:"Urdu",uz:"Uzbek",ve:"Venda",vi:"Vietnamese",vo:"Volapük",wa:"Walloon",cy:"Welsh",wo:"Wolof",fy:"Western Frisian",xh:"Xhosa",yi:"Yiddish",yo:"Yoruba",za:"Zhuang, Chuang"};
             
             $scope.currentlySearching = false; //disable form when performing a query
@@ -36,6 +43,9 @@ angular.module('sentiment', ['nvd3'])
                             //$scope.api.refresh();
                             //$scope.api.update();
                             
+                        })
+                        .error(function (response) {
+                            alert("Something didn't work. Are you sure that the server is started and listening on port 8000?")
                         });
             };
             
@@ -203,7 +213,7 @@ angular.module('sentiment', ['nvd3'])
                     dataSentiment: [],
                     refresh : function() {
                         var tmpArray= {};
-                        var tmpArraySentiment={}
+                        var tmpArraySentiment={};
                         var times = [];
                         $.each($scope.verboseTweets, function(i,data) {
                             var date = new Date(data.timestamp);
